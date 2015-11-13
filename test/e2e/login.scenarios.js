3baaa3c817d5916df6132328ab304c4e4f651618
Rory Devane <rory.devane@gmail.com>
2015-11-13 08:39:11 +0000

    added user login feature test

--- test/e2e/login.scenarios.js
+++ test/e2e/login.scenarios.js
@@ -2,9 +2,21 @@ describe('Login Application', function() {
 
     beforeEach(function () {
         browser.get('app/index.html');
-    })
-
-    it('should show the page header', function () {
-        expect(element(by.css('h1')).getText()).toContain('My Application');
     });
+
+    describe('user type user', function() {
+        it('should see the login page', function() {
+            expect(element(by.css('h2')).getText()).toContain('Please login');
+        });
+
+        it('should be able to login', function() {
+
+            element(by.css('.username')).sendKeys('user');
+            element(by.css('.password')).sendKeys('password');
+            element(by.css('.submit-button')).click();
+            expect(element(by.css('h2')).getText()).toContain('Logged In');
+
+        });
+    });
+
 });
