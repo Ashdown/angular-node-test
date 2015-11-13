22494c003a92c868d77154f97be6434176e316ee
Rory Devane <rory.devane@gmail.com>
2015-11-13 08:47:55 +0000

    added e2e test for manager user type

--- test/e2e/login.scenarios.js
+++ test/e2e/login.scenarios.js
@@ -4,14 +4,26 @@ describe('Login Application', function() {
         browser.get('app/index.html');
     });
 
-    describe('user type user', function() {
-        it('should see the login page', function() {
-            expect(element(by.css('h2')).getText()).toContain('Please Login');
-        });
+    it('should show the login page', function() {
+        expect(element(by.css('h2')).getText()).toContain('Please Login');
+    });
 
+    describe('user type user', function() {
         it('should be able to login', function() {
 
             element(by.css('.username')).sendKeys('user');
+            element(by.css('.password')).sendKeys('password');
+            element(by.css('.submit-button')).click();
+            expect(element(by.css('h2')).getText()).toContain('Logged In');
+
+        });
+    });
+
+    describe('manager type user', function() {
+
+        it('should be able to login', function() {
+
+            element(by.css('.username')).sendKeys('manager');
             element(by.css('.password')).sendKeys('password');
             element(by.css('.submit-button')).click();
             expect(element(by.css('h2')).getText()).toContain('Logged In');
